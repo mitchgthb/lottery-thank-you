78733f523db1af4fcb7eb6280fdaf9c154fc1662
mitchgthb <mitchelldpalm@hotmail.com>
2025-01-21 12:59:05 +0100

    translation

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase Configuration (Keep This File Secure)
+const firebaseConfig = {
+    apiKey: "AIzaSyAIXobiW1RMtc-la7_YErR3H5_RBCN45as",
+    authDomain: "lottery-entry.firebaseapp.com",
+    databaseURL: "https://lottery-entry-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "lottery-entry",
+    storageBucket: "lottery-entry.firebasestorage.app",
+    messagingSenderId: "262785817899",
+    appId: "1:262785817899:web:791916640532e973c3396e",
+    measurementId: "G-EF7HJMM2BN"
+};
+
+// Export Configuration
+export { firebaseConfig };
